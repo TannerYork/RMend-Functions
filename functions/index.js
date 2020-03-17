f4845d974cb3a1166b4c9e160a53d38b1281cc04
Tanner York <tannerwyork@gmail.com>
2020-03-17 17:12:30 -0500

    Add saveNewUserData firebase function

--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,36 @@
 const functions = require('firebase-functions');
+const admin = require('firebase-admin');
+const serviceAccount = require('./keys/rmend-789c8-firebase-adminsdk-hmfrb-042a92ca23.json');
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: 'https://rmend-789c8.firebaseio.com'
+});
+
+exports.saveNewUserData = functions.auth.user().onCreate(async data => {
+  admin
+    .auth()
+    .getUser(data.uid)
+    .then(user => {
+      return admin
+        .auth()
+        .setCustomUserClaims(user.uid, { authCode: '', admin: '' })
+        .then(() => {
+          return admin
+            .firestore()
+            .collection('users')
+            .doc(user.uid)
+            .set({
+              displayName: user.displayName ? user.displayName : 'New User',
+              email: user.email,
+              authCode: '',
+              admin: false,
+              id: user.uid
+            });
+        });
+    })
+    .catch(err => {
+      console.log(err.message, err.stack);
+      return { error: err.message, stack: err.stack };
+    });
+});
